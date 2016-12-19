6356ab4a45fd7df67061dc1f77900e0dbb300ba1
Kev <kev@KevdeMacBook-Pro.local>
2016-12-20 12:40:15 +1300

    app initialized

---
+++ app/homepage/homepage.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component ({
+    moduleId: module.id,
+    selector: 'home',
+    templateUrl: `homepage.component.html`,
+
+})
+
+export class HomepageComponent{}
