2c8098b668b5a218436ba7f53ef2073d889809a1
Kev <kevinzhang2012@live.cn>
2017-01-31 18:00:56 +1300

    nabar display issue fixed

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -22,14 +22,14 @@ export class DocumentsComponent {
             description: "Document is being constructing...",
             file_url:"https://github.com/kevinzhang2012",
             updated_at:"11/02/1994",
-            image_url:"https://blog.zingchart.com/content/images/2016/06/angularjsgoogle.png",
+            image_url:"http://www.devstrend.com/wp-content/uploads/2016/02/bootstrap4.jpg",
         },
         {
             title: "My third doc",
             description: "Document is being constructing...",
             file_url:"https://github.com/kevinzhang2012",
             updated_at:"11/02/1994",
-            image_url:"https://blog.zingchart.com/content/images/2016/06/angularjsgoogle.png",
+            image_url:"https://scotch.io/wp-content/uploads/2016/05/4MgyO2j7ReebGzJsLrPQ_angular-2-cli-faster-angular-projects.png",
         },
     ];
     
