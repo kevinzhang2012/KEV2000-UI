584b34354fb31789d65385739b22420e9b323559
Kev <kevinzhang2012@live.cn>
2017-02-07 16:07:28 +1300

    homepage - timeout greeting

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component ({
     moduleId: 'module.id',
@@ -6,6 +6,17 @@ import { Component } from '@angular/core';
     templateUrl: 'homepage.component.html',
 })
 
-export class HomepageComponent{
-    //Test commit
+export class HomepageComponent implements OnInit{
+    greeting : string = "WELCOME!";
+    greetings : string [] = ["WELCOME!", "Absolute", "Test"]; 
+
+    ngOnInit() : void {
+        let i = 0;
+        function TimeoutGreeting(){
+            if(i==2){
+                i=0;
+            } else {i++;}
+        }
+        setInterval(() => {this.greeting=this.greetings[i]; console.log(this.greeting);TimeoutGreeting()}, 2000);
+    }
 }
