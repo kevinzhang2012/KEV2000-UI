647fb56f105526a1298182c26f6b47bf3ef08e81
Kev <kevinzhang2012@live.cn>
2017-04-04 19:17:47 +1200

    Homepage - CSS implemented parallax effect

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -8,15 +8,15 @@ import { Component, OnInit } from '@angular/core';
 
 export class HomepageComponent implements OnInit{
     greeting : string = "WELCOME!";
-    greetings : string [] = ["WELCOME!", "Absolute", "Test"]; 
+    greetings : string [] = ["WELCOME", "Kia ora", "欢迎","Bienvenue", "ようこそ","Добро пожаловать","환영"]; 
 
     ngOnInit() : void {
         let i = 0;
         function TimeoutGreeting(){
-            if(i==2){
+            if(i==6){
                 i=0;
             } else {i++;}
         }
-        setInterval(() => {this.greeting=this.greetings[i]; console.log(this.greeting);TimeoutGreeting()}, 2000);
+        setInterval(() => {this.greeting=this.greetings[i]; console.log(this.greeting);TimeoutGreeting()}, 3000);
     }
 }
